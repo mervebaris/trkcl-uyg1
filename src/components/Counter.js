1d5c35fee1eaa2b5b49c239712110ac4ed9ea464
Merve Barış <mrvbaris1@gmail.com>
2022-09-02 11:42:22 +0300

    init

---
+++ src/components/Counter.js
@@ -0,0 +1,25 @@
+
+import React, {useState} from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const [amount, setAmount] = useState(1);
+
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + amount)}>Arttır</button>
+
+      <hr />
+
+      <div>Arttırma: + {amount}</div>
+      <button onClick={() => setAmount(1)}>+1</button>
+      <button onClick={() => setAmount(3)}>+3</button>
+      <button onClick={() => setAmount(10)}>+10</button>
+    </div>
+  );
+
+}
+
+export default Counter;
