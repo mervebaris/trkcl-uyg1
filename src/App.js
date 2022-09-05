9af1831762366d849de3327e68b3039eaf7f6896
Merve Barış <mrvbaris1@gmail.com>
2022-09-05 17:28:16 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import React, {useState} from 'react';
 import './App.css';
-import Counter from './components/Counter';
+//import Counter from './components/Counter';
 //import User from './components/User';
-import Colors from './components/Colors';
-import Form from './components/Form';
+//import Colors from './components/Colors';
+//import Form from './components/Form';
 import Users from './components/Users';
 
 function App() {
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,31 +1,52 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Users() {
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // axios("https://jsonplaceholder.typicode.com/users")
+    // .then(res => {
+    //     setUsers(res.data)
+    //    axios(`https://jsonplaceholder.typicode.com/posts?userId=${res.data[0].id}`)
+    //      .then(res => setPosts(res.data))
+    //  })
+    //   .finally(() => setLoading(false))
+    getData();
+  }, []); // componentDidMount
 
-    useEffect(() => {
-        axios("https://jsonplaceholder.typicode.com/users")
-        .then(res => setUsers(res.data))
-        .finally(() => setLoading(false))
-    },[]); // componentDidMount     
-
+  const getData = async () => {
+    const { data: users } = await axios(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    const { data: posts } = await axios(
+      `https://jsonplaceholder.typicode.com/posts?userId=${users[0].id}`
+    );
+    setLoading(false);
+    setUsers(users);
+    setPosts(posts); 
+  };
 
   return (
     <div>
+      {loading && <div>Yükleniyor...</div>}
 
-        {loading && <div>Yükleniyor...</div>}
-
-        <h2>Users</h2>
-        <ul>
-            {users.map((user) => (
-                <li key={user.id}>{user.name}</li>
-            ))}
-        </ul>
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <h2>Posts</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Users
+export default Users;
